cf4d701ccaa58f415151af4486ac13ec8a93b46f
Mihir Pudipeddi <49322476+Dubcoder@users.noreply.github.com>
2020-03-22 13:38:54 -0700

    Update Ticker.py

--- Ticker.py
+++ Ticker.py
@@ -1,6 +1,10 @@
 
-#Goal: get stock market ticker from yfinance
+#Goal: Scrape Worldometers
+#authors: Mihir, Bharadwaj, <add more>
 
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import numpy as np
 import yfinance as yf
 
 #define the ticker symbol
@@ -12,5 +16,17 @@ tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
 tickerDf = tickerData.history(period='1d', start='2020-3-1', end='2020-3-20')
 
-#see your data
-print (tickerDf)
+tCloses = tickerDf['Close'].to_list()
+
+#print(tCloses)
+
+tDates = tickerDf.index.to_list()
+print(tDates)
+
+#print(tickerDf[['Open', 'Close']]) 
+# tickerNp = tickerDf.to_numpy()
+# print(tickerNp)
+
+fig = plt.figure(figsize=(12, 6))
+plt.plot(tDates, tCloses)
+plt.show()
