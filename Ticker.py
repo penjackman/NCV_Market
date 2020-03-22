424307d75b5475ac3f6e578f00c0b661852b5ded
Mihir Pudipeddi <49322476+Dubcoder@users.noreply.github.com>
2020-03-22 10:47:49 -0700

    Ticker gets stock market data.

---
+++ Ticker.py
@@ -0,0 +1,18 @@
+
+#Goal: Scrape Worldometers
+#authors: Mihir, Bharadwaj, <add more>
+
+
+import yfinance as yf
+
+#define the ticker symbol
+tickerSymbol = 'AAPL'
+
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2020-3-1', end='2020-3-20')
+
+#see your data
+print (tickerDf)
